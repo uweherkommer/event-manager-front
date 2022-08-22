2c8932c84ae4d3f5e7d3d448b0529f21b628491a
uweherkommer <103587904+uweherkommer@users.noreply.github.com>
2022-08-22 20:32:42 +0200

    uwe home of events

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -4,7 +4,7 @@ import { Events } from '../events/Events';
 const Homepage = () => {
   return (
     <div className={styles.homepage}>
-      <h1> Your home of events</h1>
+      <h1> Uwe home of events</h1>
       <div>
         <input
           type="text"
@@ -13,6 +13,7 @@ const Homepage = () => {
           className={styles.search_bar}
         />
       </div>
+      
       <div className={styles.search_icon_wrapper}>
         <img src="https://res.cloudinary.com/imichaelowolabi/image/upload/v1657045124/pexels-in-cloudinary/event-manager-redis/search1.png" className={styles.search_icon} alt="search" />
       </div>
